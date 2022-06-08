809b5b8c5cd6535ff79a907497d8edb7216878fd
Deivid Andreis <obreezygamer@gmail.com>
2022-06-08 07:32:23 -0300

    versao video

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ let img = document.querySelector('#img');
 let ligar = document.querySelector('#ligar');
 let desligar = document.querySelector('#desligar');
 
+function isLampBroken(){
+    return img.src.indexOf('quebrada') >-1
+}
+
 function ligarLampada(){
-    let img = document.querySelector('#img').src="./img/ligada.jpg";
+    if(!isLampBroken()){    
+     let img = document.querySelector('#img').src="./img/ligada.jpg";
+    }
 }
 
 function desligarLampada(){
+    if(!isLampBroken()){  
     let img = document.querySelector('#img').src="./img/desligada.jpg";
+    }
 }
 
 function lampadaQuebrada(){
+    if(!isLampBroken()){  
     let img = document.querySelector('#img').src="./img/quebrada.jpg";
+    }
 
 }
 
@@ -20,4 +30,6 @@ function lampadaQuebrada(){
 
 ligar.addEventListener('click', ligarLampada, false);
 desligar.addEventListener('click', desligarLampada, false);
-img.addEventListener('dblclick', lampadaQuebrada, false);
+img.addEventListener('dblclick', lampadaQuebrada, false);  
+img.addEventListener('mouseover', ligarLampada);
+img.addEventListener('mouseleave', desligarLampada);
